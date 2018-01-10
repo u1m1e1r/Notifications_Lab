f17560f903a6440c0e12614c5bf9f17d2102bfeb
Omar676767 <35200404+Omar676767@users.noreply.github.com>
2018-01-11 01:23:19 +0500

    Initial commit

---
+++ app/src/main/java/com/example/salman/madlabnotifications/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.salman.madlabnotifications;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Salman on 12/6/2017.
+ */
+
+class MainActivity2 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_main);
+
+    }
+}
